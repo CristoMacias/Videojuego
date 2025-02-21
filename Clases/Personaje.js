919b25883a8ef14cedc141ac775da99b96b0cc3e
smmonino <smmoninog01@iesalbarregas.es>
2025-02-21 17:26:32 +0100

    Modificar Personaje

--- Clases/Personaje.js
+++ Clases/Personaje.js
@@ -1,23 +1,29 @@
 
 export default class Personaje{
     
-    nombre;
-    raza;
-    vida;
-    mana;
-    ataque;
-    defensa;
-    magia;
-    resitenciaMagica;
-    nivel;
-    experiencia;
-    oro;
-
-    constructor(nombre,raza,max,max,ataque,defensa,magia,resitenciaMagica){
+    nombre; //Nombre del personaje
+    raza;//Raza: Enano,Elfo,Humano,Mago,Orco,
+    vida;//Vida máxima y vida actual
+    mana;//Maná máximo, y maná actual
+    ataque;//Puntos de ataque
+    defensa;//Puntos de defensa
+    magia;//Puntos de magia
+    resitenciaMagica;//Puntos de resistencia mágica
+    nivel;//Nivel actual
+    experiencia;//Experiencia
+    oro; // Total de oro 
+    imagen; // Imagen elegida
+    
+    //Constructor creador de personajes
+    constructor(nombre,raza,max,ataque,defensa,magia,resistenciaMagica,imagen){
         this.nombre = nombre;
         this.raza=raza;
-        this.vida = {max: 150,actual:max};
-        this.mana = {max: 60, actual: max};
-        thia.ataque = 90;
+        this.vida = {max: max,actual:max};
+        this.mana = {max: max, actual: max};
+        this.ataque = ataque;
+        this.defensa= defensa;
+        this.magia=magia;
+        this.resistenciaMagica=resistenciaMagica;
+        this.imagen=imagen;
     }
 }
