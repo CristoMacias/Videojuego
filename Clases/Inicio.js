273748aa3fa5f29cdb409c4164cf7858ae2817ed
Cristo Macias <cmaciasi01@iesalbarregas.es>
2025-03-06 20:39:35 +0100

    Inicio terminado

---
+++ Clases/Inicio.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    const continuarPartida = document.querySelector("#continuarPartida");
+    const eliminarDatos = document.querySelector("#eliminarDatos");
+    
+    localStorage.setItem("personajes", "Hola");
+    //localStorage.splice("personajes", "Hola");
+
+    if(localStorage.length === 0){
+        continuarPartida.classList.add("noPersonaje");
+        eliminarDatos.classList.add("noPersonaje");
+    }
+    else{
+        continuarPartida.classList.remove("noPersonaje");
+        eliminarDatos.classList.remove("noPersonaje");
+    }
+
+    
+
+});
+
+
+
