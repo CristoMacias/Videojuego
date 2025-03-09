7c814d8a406fc8a602b5f9cdd7e518a1a22b2469
Cristo Macias <cmaciasi01@iesalbarregas.es>
2025-03-09 20:48:45 +0100

    lobby creado

---
+++ Clases/Lobby.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+
+    //Atributos parte superior
+    const spanVida = document.querySelector("#spanVida");
+    const spanMana = document.querySelector("#spanMana");
+    const spanDanioFisico = document.querySelector("#spanDanioFisico");
+    const spanArmadura = document.querySelector("#spanArmadura");
+    const spanOro = document.querySelector("#spanOro");
+
+    //Imagen
+    const imagen = document.querySelector("#imagen");
+
+    //Atributos parte inferior
+    const spanNombre = document.querySelector("#spanNombre");
+    const spanRaza = document.querySelector("#spanRaza");
+    const spanNivel = document.querySelector("#spanNivel");
+    const spanExperiencia = document.querySelector("#spanExperiencia");
+    const spanPoderMagico = document.querySelector("#spanPoderMagico")
+    const spanResistenciaMagica = document.querySelector("#spanResistenciaMagica")
+
+    const personaje = JSON.parse(localStorage.getItem('personaje'));
+
+    if(personaje){
+
+    spanVida.textContent = " : " + personaje.vida.actual;
+    spanMana.textContent = " : " + personaje.mana.actual;
+    spanDanioFisico.textContent = " : " + personaje.ataque;
+    spanArmadura.textContent = " : " + personaje.defensa;
+    spanPoderMagico.textContent = " : " + personaje.magia;
+    spanResistenciaMagica.textContent = " : " + personaje.resistenciaMagica;
+
+    spanNombre.textContent = " : " + personaje.nombre;
+    spanRaza.textContent = " : " + personaje.raza;
+    spanNivel.textContent = " : " + personaje.nivel;
+    spanExperiencia.textContent = " : " + personaje.experiencia; 
+    spanOro.textContent = " : " + personaje.oro;
+
+    imagen.src = personaje.imagen;
+    
+    }
+});
