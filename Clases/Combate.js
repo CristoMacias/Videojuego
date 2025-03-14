3feb5a2d2e85de9b21b5055587626ad54bf5739a
Cristo Macias <cmaciasi01@iesalbarregas.es>
2025-03-14 16:24:47 +0100

    Cambate

--- Clases/Combate.js
+++ Clases/Combate.js
@@ -1,6 +1,6 @@
 import Personaje from "./Personaje.js";
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     const jsonpersonaje = JSON.parse(localStorage.getItem('personaje'));
     const personaje = Personaje.reconstruirJson(jsonpersonaje);
@@ -24,49 +24,49 @@ document.addEventListener('DOMContentLoaded',()=>{
     const botonHuir = document.querySelector("#botonHuir");
 
 
-    if(personaje){
-    //Mostrar los atributos de personajes
-    spanNombreAliado.textContent = " : " + personaje.nombre;
-    spanNombreEnemigo.textContent = " : " + enemigo.nombre;
-    spanNivelAliado.textContent = " : " + personaje.nivel;
-    spanNivelEnemigo.textContent = " : " + enemigo.nivel;
-    spanVidaAliado.textContent = " : " + personaje.vidaActual;
-    spanVidaEnemigo.textContent = " : " + enemigo.vida;
-    mana.textContent = " : " + personaje.manaActual;
+    if (personaje) {
+        //Mostrar los atributos de personajes
+        spanNombreAliado.textContent = " : " + personaje.nombre;
+        spanNombreEnemigo.textContent = " : " + enemigo.nombre;
+        spanNivelAliado.textContent = " : " + personaje.nivel;
+        spanNivelEnemigo.textContent = " : " + enemigo.nivel;
+        spanVidaAliado.textContent = " : " + personaje.vidaActual;
+        spanVidaEnemigo.textContent = " : " + enemigo.vida;
+        mana.textContent = " : " + personaje.manaActual;
     }
-  
+
     let contadorCriticoEnemigo = Math.floor(Math.random() * 10);
 
     /**
      * Método para atacar
      */
-    const atacar = function (){
-        
-        if(enemigo.vida > 0){
-        enemigo.vida -= personaje.ataque;
-        }
+    const atacar = function () {
 
-        if(contadorCriticoEnemigo == 4){
+        if (enemigo.vida > 0) {
+            enemigo.vida -= personaje.ataque;
+        }
+        spanVidaEnemigo.textContent = " : " + enemigo.vida;
+        if (contadorCriticoEnemigo == 4) {
             setTimeout(() => {
                 personaje.vidaActual -= enemigo.ataque + 50;
-                spanVidaAliado.textContent = " : " + personaje.vidaActual; 
+                spanVidaAliado.textContent = " : " + personaje.vidaActual;
                 alert("El enemigo te ha atacado");
-                }, 500);
-        }else{
-        setTimeout(() => {
-        personaje.vidaActual -= enemigo.ataque
-        spanVidaAliado.textContent = " : " + personaje.vidaActual; 
-        alert("El enemigo te ha atacado");
-        }, 500);
+            }, 500);
+        } else {
+            setTimeout(() => {
+                personaje.vidaActual -= enemigo.ataque
+                spanVidaAliado.textContent = " : " + personaje.vidaActual;
+                alert("El enemigo te ha atacado");
+            }, 500);
         }
-        
+
     }
     botonAtacar.addEventListener("click", atacar);
-    
+
     /**
      * Método de defender
      */
-    const defender = function(){
+    const defender = function () {
         console.log("Esta función aun no está implementada.")
     }
     botonDefender.addEventListener("click", defender);
@@ -74,32 +74,32 @@ document.addEventListener('DOMContentLoaded',()=>{
     /**
      * Método para dar un golpe crítico
      */
-    const golpeCritico = function(){
-        if(enemigo.vida > 0){
+    const golpeCritico = function () {
+
+        if (enemigo.vida > 0) {
             enemigo.vida -= personaje.ataque + 50;
             personaje.manaActual -= 20;
-            }
-    
-            spanVidaEnemigo.textContent = " : " + enemigo.vida;
-            mana.textContent = " : " + personaje.manaActual;
+        }
+        spanVidaEnemigo.textContent = " : " + enemigo.vida;
+        mana.textContent = " : " + personaje.manaActual;
 
-            setTimeout(() => {
+        setTimeout(() => {
             personaje.vidaActual -= enemigo.ataque
-            spanVidaAliado.textContent = " : " + personaje.vidaActual; 
+            spanVidaAliado.textContent = " : " + personaje.vidaActual;
             alert("El enemigo te ha atacado");
-            }, 500);
+        }, 500);
     }
     botonCritico.addEventListener("click", golpeCritico);
 
     /**
      * Botón de tomar poción.
      */
-    const tomarPocion = function(){
-        
+    const tomarPocion = function () {
+
     }
     botonPocion.addEventListener("click", tomarPocion)
 
-    const huirDelCombate = function(){
+    const huirDelCombate = function () {
         localStorage.removeItem('enemigo');
     }
     botonHuir.addEventListener("click", huirDelCombate);
